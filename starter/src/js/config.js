514b77685283189fd9081d1b1177915f0d8d6eaf
hadipouranian <hadi.pouranian@gmail.com>
2021-04-04 20:29:52 +0800

    initial commit

---
+++ starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ff9f562e-0ae6-4f4e-b0c2-17911ac5810c';
+export const MODAL_CLOSE_SEC = 2.5;
